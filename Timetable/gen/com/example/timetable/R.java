9787deb29427172f160e80eb02b4fcc06d7d7c9f
mengyangwang <mengyangwang101@gmail.com>
2014-04-09 15:44:20 +0800

    ui demo

--- Timetable/gen/com/example/timetable/R.java
+++ Timetable/gen/com/example/timetable/R.java
@@ -21,16 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int shapee=0x7f020001;
-        public static final int table_bg=0x7f020002;
-        public static final int txtview_border=0x7f020003;
+        public static final int head_bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int pink_bg=0x7f020002;
+        public static final int purplebg=0x7f020003;
+        public static final int shapee=0x7f020004;
+        public static final int table_bg=0x7f020005;
+        public static final int txtview_border=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int tableAc_days=0x7f080000;
-        public static final int tableAc_table=0x7f080002;
-        public static final int tableAc_time=0x7f080001;
+        public static final int tableAc_middle=0x7f080001;
+        public static final int tableAc_table=0x7f080003;
+        public static final int tableAc_time=0x7f080002;
     }
     public static final class layout {
         public static final int table_activity=0x7f030000;
--- Timetable/src/com/wang/activity/TableActivity.java
+++ Timetable/src/com/wang/activity/TableActivity.java
@@ -1,17 +1,24 @@
 package com.wang.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.timetable.R;
+import com.wang.service.ImgService;
 
 public class TableActivity extends Activity {
 	private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
@@ -20,31 +27,63 @@ public class TableActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		int screenWidth = (getWindowManager().getDefaultDisplay().getWidth()-30);	
-		int screenHeight = getWindowManager().getDefaultDisplay().getHeight();	
+		int screenWidth = (getWindowManager().getDefaultDisplay().getWidth() - 30);
+		int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
 		setContentView(R.layout.table_activity);
 		TableLayout tableLayout = (TableLayout) findViewById(R.id.tableAc_table);
-		// 全部列自动填充空白处
-		// 生成10行，8列的表格
+		RelativeLayout reLayout = (RelativeLayout) findViewById(R.id.tableAc_time);
+		RelativeLayout midLayout = (RelativeLayout) findViewById(R.id.tableAc_middle);
 		for (int row = 0; row < 13; row++) {
+			int hgt = 0;
 			TableRow tableRow = new TableRow(this);
 			for (int col = 0; col < 7; col++) {
 				TextView tv = new TextView(this);
 				tv.setBackgroundResource(R.drawable.shapee);
-				tv.setHeight(screenHeight/13);
-				tv.setWidth(screenWidth/7);
+				tv.setHeight(screenHeight / 13);
+				tv.setWidth(screenWidth / 7);
+				System.out.println(screenWidth/7);
+				hgt = screenHeight / 13;
 				tv.getBackground().setAlpha(200);
 				tableRow.addView(tv);
 			}
-			// 新建的TableRow添加到TableLayout
 			tableLayout.addView(tableRow, new TableLayout.LayoutParams(FP, WC));
+			if (row > 0 && row < 13) {
+				TextView tv = new TextView(this);
+				tv.setWidth(30);
+				tv.setGravity(Gravity.CENTER);
+				Integer txt = 8 + row;
+				tv.setText(txt.toString());
+				tv.setY(hgt * row - 10);
+				System.out.println(hgt*row);
+				reLayout.addView(tv);
+			}
 		}
+		RelativeLayout course = new RelativeLayout(this);
+		RelativeLayout.LayoutParams itemParam = new RelativeLayout.LayoutParams(
+				41, 72);
+		course.setLayoutParams(itemParam);
+		course.setX(30);
+		course.setY(36);
 		
-		LinearLayout linearLayout =(LinearLayout)findViewById(R.id.tableAc_time);
-		TextView tv = new TextView(this);
-		tv.setGravity(Gravity.RIGHT);
-		tv.setText("8");
-		linearLayout.addView(tv);
+		Drawable btnDrawable = getBaseContext().getResources().getDrawable(R.drawable.pink_bg);
+		BitmapDrawable bd = (BitmapDrawable) btnDrawable;
+		Bitmap bm = bd.getBitmap();
+		ImageView dd = new ImageView(this);
+		dd.setPadding(1, 1, 1, 1);
+		dd.setImageBitmap(ImgService.createFramedPhoto(41,72,bm,5));
+		RelativeLayout.LayoutParams em =new RelativeLayout.LayoutParams(WC, WC);
+		course.addView(dd,em);
+		TextView tv= new TextView(this);
+		tv.setText("高数");
+		tv.setWidth(41);
+		tv.setHeight(72);
+		tv.setPadding(1, 1, 1, 1);
+		tv.setTextColor(Color.WHITE);
+		tv.setGravity(Gravity.CENTER);
+		tv.setTextSize(10);
+		course.addView(tv,em);
+		
+		midLayout.addView(course, new RelativeLayout.LayoutParams(WC, WC));
 	}
 
 }
